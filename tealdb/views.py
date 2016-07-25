01ef3a157cc616210d87da990ca4621a2b15185c
Dan Lowe <dan@tangledhelix.com>
2016-07-25 12:44:21 -0400

    Implement add site to the database

--- tealdb/views.py
+++ tealdb/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from tealdb.models import Site, Contact
+from tealdb import views
 
 
 def main(request):
@@ -18,7 +21,9 @@ def add_contact(request):
 
 
 def sites(request):
-    return render(request, 'sites.html')
+    sites = Site.objects.all()
+    context = {'sites': sites}
+    return render(request, 'sites.html', context)
 
 
 def site(request, site_id):
@@ -26,4 +31,53 @@ def site(request, site_id):
 
 
 def add_site(request):
-    return render(request, 'add_site.html')
+    error = None
+    context = {}
+
+    if request.method == 'POST':
+        if 'inputName' not in request.POST:
+            error = 'Site name is required.'
+        elif not request.POST['inputName'].strip():
+            error = 'Site name is required.'
+
+        if error:
+            context['error'] = error
+        else:
+            site = Site()
+            site.name = request.POST['inputName'].strip()
+            if 'inputAddress1' in request.POST:
+                site.address1 = request.POST['inputAddress1'].strip()
+            if 'inputAddress2' in request.POST:
+                site.address2 = request.POST['inputAddress2'].strip()
+            if 'inputCity' in request.POST:
+                site.city = request.POST['inputCity'].strip()
+            if 'inputState' in request.POST:
+                site.state = request.POST['inputState'].strip()
+            if 'inputPostalCode' in request.POST:
+                site.postal_code = request.POST['inputPostalCode'].strip()
+            if 'inputCountry' in request.POST:
+                site.country = request.POST['inputCountry'].strip()
+            if 'inputWebSite' in request.POST:
+                site.web_site = request.POST['inputWebSite'].strip()
+            if 'inputEmail' in request.POST:
+                site.email = request.POST['inputEmail'].strip()
+            if 'inputPhone' in request.POST:
+                site.phone = request.POST['inputPhone'].strip()
+            if 'inputFax' in request.POST:
+                site.fax = request.POST['inputFax'].strip()
+            if 'inputYearsLit' in request.POST:
+                site.years_lit = request.POST['inputYearsLit'].strip()
+            if 'inputMapLink' in request.POST:
+                site.map_link = request.POST['inputMapLink'].strip()
+            if 'inputNotes' in request.POST:
+                site.notes = request.POST['inputNotes'].strip()
+            if 'inputDisposition' in request.POST:
+                site.disposition = request.POST['inputDisposition'].strip()
+            if 'inputFee' in request.POST:
+                site.fee = True
+            if 'inputHaveLit' in request.POST:
+                site.have_lit = True
+            site.save()
+            return redirect(views.sites)
+
+    return render(request, 'add_site.html', context)
