171434597b3d1cfcf7d03b838d8bce784ce6e7ff
Dan Lowe <dan@tangledhelix.com>
2016-07-25 10:33:15 -0400

    Add a generic main page

--- turnitteal/urls.py
+++ turnitteal/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from tealdb import views
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', views.main, name='main'),
 ]
